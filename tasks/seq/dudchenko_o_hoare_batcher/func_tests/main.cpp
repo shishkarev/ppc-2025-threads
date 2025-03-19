617817caab0b15c84d7c059d5b7cca7f291ffb54
shishkarev <and01072004@gmail.com>
2025-03-19 03:31:10 +0300

    fix build windows

--- tasks/seq/dudchenko_o_hoare_batcher/func_tests/main.cpp
+++ tasks/seq/dudchenko_o_hoare_batcher/func_tests/main.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 
+#include <algorithm>
 #include <cstddef>
 #include <cstdint>
 #include <fstream>
--- tasks/seq/dudchenko_o_hoare_batcher/perf_tests/main.cpp
+++ tasks/seq/dudchenko_o_hoare_batcher/perf_tests/main.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 
+#include <algorithm>
 #include <chrono>
 #include <cstddef>
 #include <cstdint>
