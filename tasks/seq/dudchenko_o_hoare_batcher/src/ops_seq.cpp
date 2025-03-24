52a6591f7cbe52e27c08cb91cd0ddc147fec4871
shishkarev <and01072004@gmail.com>
2025-03-24 23:13:32 +0300

    2nd fix build after changes from 2nd review

--- tasks/seq/dudchenko_o_hoare_batcher/src/ops_seq.cpp
+++ tasks/seq/dudchenko_o_hoare_batcher/src/ops_seq.cpp
@@ -25,8 +25,11 @@ bool dudchenko_o_hoare_batcher_seq::TestTaskSequential::ValidationImpl() {
 }
 
 bool dudchenko_o_hoare_batcher_seq::TestTaskSequential::RunImpl() {
+  if (input_.empty()) {
+    return false;
+  }
+
   QuickSort(input_, 0, static_cast<int>(input_.size()) - 1);
-  output_.clear();
   output_ = input_;
   return true;
 }
