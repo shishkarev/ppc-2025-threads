865e315b1a87fe25903f11d3dde1d0a8e6a7d93b
shishkarev <and01072004@gmail.com>
2025-04-03 04:28:03 +0300

    fix msvc build and seg-fault in tests

--- tasks/omp/shishkarev_a_gift_wraping_algorithm/src/ops_omp.cpp
+++ tasks/omp/shishkarev_a_gift_wraping_algorithm/src/ops_omp.cpp
@@ -15,13 +15,13 @@ std::vector<shishkarev_a_gift_wraping_algorithm_omp::Vertex> shishkarev_a_gift_w
 }
 
 bool shishkarev_a_gift_wraping_algorithm_omp::TestTaskOpenMP::PreProcessingImpl() {
-  unsigned int input_size = task_data->inputs_count[0];
+  int input_size = static_cast<int>(task_data->inputs_count[0]);
   auto* in_ptr = reinterpret_cast<Vertex*>(task_data->inputs[0]);
   input_ = std::vector<Vertex>(in_ptr, in_ptr + input_size);
 
   input_ = RemoveDuplicates(input_);
 
-  unsigned int output_size = task_data->outputs_count[0];
+  int output_size = static_cast<int>(task_data->outputs_count[0]);
   output_ = std::vector<Vertex>(output_size, {0, 0});
 
   rc_size_ = static_cast<int>(std::sqrt(input_size));
@@ -35,8 +35,8 @@ bool shishkarev_a_gift_wraping_algorithm_omp::TestTaskOpenMP::ValidationImpl() {
 bool shishkarev_a_gift_wraping_algorithm_omp::TestTaskOpenMP::RunImpl() {
   if (input_.size() < 3) {
     output_.clear();
-    for (size_t i = 1; i < input_.size(); ++i) {
-      output_[i] = input_[i];
+    for (int i = 0; i < static_cast<int>(input_.size()); ++i) {
+      output_.push_back(input_[i]);
     }
     return true;
   }
@@ -44,28 +44,51 @@ bool shishkarev_a_gift_wraping_algorithm_omp::TestTaskOpenMP::RunImpl() {
   output_.clear();
 
   int start_point = 0;
-#pragma omp parallel for reduction(min : start_point)
-  for (size_t i = 1; i < input_.size(); ++i) {
-    if ((input_[i].y < input_[start_point].y) ||
-        ((input_[i].y == input_[start_point].y) && (input_[i].x > input_[start_point].x))) {
-      start_point = static_cast<int>(i);
+#pragma omp parallel
+  {
+    int local_start = start_point;
+#pragma omp for nowait
+    for (int i = 1; i < static_cast<int>(input_.size()); ++i) {
+      if ((input_[i].y < input_[local_start].y) ||
+          ((input_[i].y == input_[local_start].y) && (input_[i].x > input_[local_start].x))) {
+        local_start = i;
+      }
+    }
+#pragma omp critical
+    {
+      if ((input_[local_start].y < input_[start_point].y) ||
+          ((input_[local_start].y == input_[start_point].y) && (input_[local_start].x > input_[start_point].x))) {
+        start_point = local_start;
+      }
     }
   }
 
   int p = start_point;
   do {
-    output_.push_back(input_[p]);
-    size_t q = (p + 1) % input_.size();
-
-#pragma omp parallel for
-    for (size_t i = 0; i < input_.size(); ++i) {
-      const auto angle = input_[p].Angle(input_[q], input_[i]);
-      if (angle < 0 || (angle == 0 && input_[p].Length(input_[i]) > input_[p].Length(input_[q]))) {
 #pragma omp critical
-        { q = i; }
+    output_.push_back(input_[p]);  
+
+    int q = (p + 1) % static_cast<int>(input_.size());
+
+#pragma omp parallel
+    {
+      int local_q = q;
+#pragma omp for nowait
+      for (int i = 0; i < static_cast<int>(input_.size()); ++i) {
+        const auto angle = input_[p].Angle(input_[q], input_[i]);
+        if (angle < 0 || (angle == 0 && input_[p].Length(input_[i]) > input_[p].Length(input_[q]))) {
+          local_q = i;
+        }
+      }
+#pragma omp critical
+      {
+        const auto angle = input_[p].Angle(input_[q], input_[local_q]);
+        if (angle < 0 || (angle == 0 && input_[p].Length(input_[local_q]) > input_[p].Length(input_[q]))) {
+          q = local_q;
+        }
       }
     }
-    p = static_cast<int>(q);
+    p = q;
   } while (p != start_point);
 
   return true;
@@ -74,8 +97,11 @@ bool shishkarev_a_gift_wraping_algorithm_omp::TestTaskOpenMP::RunImpl() {
 bool shishkarev_a_gift_wraping_algorithm_omp::TestTaskOpenMP::PostProcessingImpl() {
   auto* out_ptr = reinterpret_cast<Vertex*>(task_data->outputs[0]);
 
-  size_t min_size = std::min(output_.size(), static_cast<size_t>(task_data->outputs_count[0]));
-  std::copy(output_.begin(), output_.begin() + min_size, out_ptr);
+  int min_size = std::min(static_cast<int>(output_.size()), static_cast<int>(task_data->outputs_count[0]));
+
+  if (output_.size() >= min_size) {
+    std::copy(output_.begin(), output_.begin() + min_size, out_ptr);
+  }
 
   return true;
 }
