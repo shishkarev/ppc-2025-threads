64571e257c1cdfa47af2181436fba020461c3d88
shishkarev <and01072004@gmail.com>
2025-04-10 15:54:46 +0300

    fix clang-format after fix tests

--- tasks/omp/shishkarev_a_gift_wraping_algorithm/src/ops_omp.cpp
+++ tasks/omp/shishkarev_a_gift_wraping_algorithm/src/ops_omp.cpp
@@ -38,21 +38,20 @@ bool shishkarev_a_gift_wraping_algorithm_omp::TestTaskOpenMP::RunImpl() {
   output_.clear();
 
   int start_point = 0;
-  #pragma omp parallel
+#pragma omp parallel
   {
     int local_start = start_point;
-    #pragma omp for nowait
+#pragma omp for nowait
     for (int i = 1; i < static_cast<int>(input_.size()); ++i) {
       if ((input_[i].y < input_[local_start].y) ||
           ((input_[i].y == input_[local_start].y) && (input_[i].x > input_[local_start].x))) {
         local_start = i;
       }
     }
-    #pragma omp critical
+#pragma omp critical
     {
       if ((input_[local_start].y < input_[start_point].y) ||
-          ((input_[local_start].y == input_[start_point].y) && 
-           (input_[local_start].x > input_[start_point].x))) {
+          ((input_[local_start].y == input_[start_point].y) && (input_[local_start].x > input_[start_point].x))) {
         start_point = local_start;
       }
     }
@@ -68,48 +67,48 @@ bool shishkarev_a_gift_wraping_algorithm_omp::TestTaskOpenMP::RunImpl() {
     std::vector<int> local_qs;
     int initial_candidate = (p + 1) % static_cast<int>(input_.size());
     if (initial_candidate == p && input_.size() > 1) {
-       initial_candidate = (p + 2) % static_cast<int>(input_.size());
+      initial_candidate = (p + 2) % static_cast<int>(input_.size());
     }
-    if (input_.size() <= 1) initial_candidate = p;
-    else if (initial_candidate == p) initial_candidate = (p > 0) ? 0 : 1;
+    if (input_.size() <= 1)
+      initial_candidate = p;
+    else if (initial_candidate == p)
+      initial_candidate = (p > 0) ? 0 : 1;
 
 
-    #pragma omp parallel
+#pragma omp parallel
     {
-        int num_threads = omp_get_num_threads();
-        int thread_id = omp_get_thread_num();
+      int num_threads = omp_get_num_threads();
+      int thread_id = omp_get_thread_num();
 
-        #pragma omp single
-        {
-            local_qs.resize(num_threads);
+#pragma omp single
+      { local_qs.resize(num_threads); }
+      int thread_local_q = initial_candidate;
+
+#pragma omp for nowait
+      for (int i = 0; i < static_cast<int>(input_.size()); ++i) {
+        if (i == p) continue;
+
+        const auto angle = input_[p].Angle(input_[thread_local_q], input_[i]);
+
+        if (angle < 0 || (angle == 0 && input_[p].Length(input_[i]) > input_[p].Length(input_[thread_local_q]))) {
+          thread_local_q = i;
         }
-        int thread_local_q = initial_candidate;
+      }
 
-        #pragma omp for nowait
-        for (int i = 0; i < static_cast<int>(input_.size()); ++i) {
-            if (i == p) continue;
+      local_qs[thread_id] = thread_local_q;
 
-            const auto angle = input_[p].Angle(input_[thread_local_q], input_[i]);
+#pragma omp barrier
 
-            if (angle < 0 || (angle == 0 && input_[p].Length(input_[i]) > input_[p].Length(input_[thread_local_q]))) {
-                 thread_local_q = i;
-            }
+#pragma omp single
+      {
+        q = local_qs[0];
+        for (int k = 1; k < num_threads; ++k) {
+          const auto angle = input_[p].Angle(input_[q], input_[local_qs[k]]);
+            if (angle < 0 || (angle == 0 && input_[p].Length(input_[local_qs[k]]) > input_[p].Length(input_[q]))) {
+              q = local_qs[k];
+          }
         }
-
-        local_qs[thread_id] = thread_local_q;
-
-        #pragma omp barrier
-
-        #pragma omp single
-        {
-            q = local_qs[0];
-            for (int k = 1; k < num_threads; ++k) {
-                const auto angle = input_[p].Angle(input_[q], input_[local_qs[k]]);
-                 if (angle < 0 || (angle == 0 && input_[p].Length(input_[local_qs[k]]) > input_[p].Length(input_[q]))) {
-                    q = local_qs[k];
-                }
-            }
-        }
+      }
     }
 
      if (q == -1 || q == p) {
