5fcc941bba3361b7aab9a5bc95143109e93d7ef5
shishkarev <and01072004@gmail.com>
2025-04-10 16:19:43 +0300

    fix clang-format after remade validation

--- tasks/omp/shishkarev_a_gift_wraping_algorithm/src/ops_omp.cpp
+++ tasks/omp/shishkarev_a_gift_wraping_algorithm/src/ops_omp.cpp
@@ -26,8 +26,8 @@ bool shishkarev_a_gift_wraping_algorithm_omp::TestTaskOpenMP::PreProcessingImpl(
 }
 
 bool shishkarev_a_gift_wraping_algorithm_omp::TestTaskOpenMP::ValidationImpl() {
-  if (task_data->inputs_count.size() != 1 || task_data->outputs_count.size() != 1 ||
-      task_data->inputs.size() != 1 || task_data->outputs.size() != 1) {
+  if (task_data->inputs_count.size() != 1 || task_data->outputs_count.size() != 1 || task_data->inputs.size() != 1 ||
+      task_data->outputs.size() != 1) {
     return false;
   }
 
